607a29bb012ab89be093b7c77e09e992085f79a2
kyoryo <franz.rio19@gmail.com>
2021-07-30 16:27:03 +0700

    prop name json

--- Source/Source/Models/QuestionModel.cs
+++ Source/Source/Models/QuestionModel.cs
@@ -1,31 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Source.Models
 {
     public class QuestionModel : Item
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("category")]
         public int Category { get; set; }
+        [JsonPropertyName("tags")]
         public List<string> Tags { get; set; } = new List<string>();
+        [JsonPropertyName("createdAt")]
         public DateTime CreatedAt { get; set; }
     }
 
     public class QuestionThreeModel
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("category")]
         public int Category { get; set; }
+        [JsonPropertyName("items")]
         public List<Item> Items { get; set; }
+        [JsonPropertyName("createdAt")]
         public DateTime CreatedAt { get; set; }
+        [JsonPropertyName("tags")]
         public List<string> Tags { get; set; } = new List<string>();
     }
 
     public class Item
     {
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+        [JsonPropertyName("footer")]
         public string Footer { get; set; }
 
     }
